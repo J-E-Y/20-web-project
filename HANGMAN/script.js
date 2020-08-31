3ea94376d3a258dbe5895d6861c28d8f000676ec
J-E-Y <rightfuljung@gmail.com>
2020-08-31 13:41:03 +0900

    fix from helloWorld to helloworld

--- HANGMAN/script.js
+++ HANGMAN/script.js
@@ -7,7 +7,7 @@ const finalMessage = document.getElementById('final-message');
 
 const figureParts = document.querySelectorAll('.figure-part');
 
-const words = ['helloWorld', 'hangman', 'bible', 'jj'];
+const words = ['helloworld', 'hangman', 'bible', 'jj'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
