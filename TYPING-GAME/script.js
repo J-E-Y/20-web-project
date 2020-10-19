bf6e1347ccc5c1baba72922f53e4d21cc053a4ed
J-E-Y <rightfuljung@gmail.com>
2020-10-19 16:42:01 +0900

    Adding The Timer

--- TYPING-GAME/script.js
+++ TYPING-GAME/script.js
@@ -42,6 +42,13 @@ let score = 0;
 //? Init time
 let time = 10;
 
+//? Focus on text on start
+text.focus();
+
+//? Start counting down
+
+const timeInterval = setInterval(updateTime, 1000);
+
 //? Generate random word from array
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
@@ -60,6 +67,29 @@ function updateScore() {
   scoreEl.innerHTML = score;
 }
 
+//? Update time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + 's';
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    //? end game
+    gameOver();
+  }
+}
+
+//? Game over, show screen
+function gameOver() {
+  endgameEl.innerHTML = `
+  <h1>Time ran out</h1>
+  <p> Your final scroe is ${score}</p>
+  <button onclick="location.reload()">Reload</button>
+  `;
+
+  endgameEl.style.display = 'flex';
+}
+
 addWordToDOM();
 
 //? Event listeners
@@ -70,7 +100,11 @@ text.addEventListener('input', (e) => {
   if (insertedText === randomWord) {
     addWordToDOM();
     updateScore();
+
     //? Clear
     e.target.value = '';
+    time += 2;
+
+    updateTime();
   }
 });
