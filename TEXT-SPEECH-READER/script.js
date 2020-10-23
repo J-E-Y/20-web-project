3b2cbc921b1c9302f986e19a96f6051fd1dcd03c
J-E-Y <rightfuljung@gmail.com>
2020-10-23 18:24:31 +0900

    Get Speech Voices

--- TEXT-SPEECH-READER/script.js
+++ TEXT-SPEECH-READER/script.js
@@ -75,3 +75,33 @@ function createBox(item) {
 
   main.appendChild(box);
 }
+
+//? Store voices
+let voices = [];
+
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach((voice) => {
+    const option = document.createElement('option');
+    option.vlaue = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+    voicesSelect.appendChild(option);
+  });
+}
+
+//? Voices changed
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+//? Toggle text box
+
+toggleBtn.addEventListener('click', () => {
+  document.getElementById('text-box').classList.toggle('show');
+});
+
+//? Close button
+
+closeBtn.addEventListener('click', () => {
+  document.getElementById('text-box').classList.remove('show');
+});
+
+getVoices();
