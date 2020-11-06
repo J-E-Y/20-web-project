8a15143b2e93115756638856c33e0c2d49fca5ee
J-E-Y <rightfuljung@gmail.com>
2020-11-06 10:49:21 +0900

    Capture Mic Input

---
+++ SPEAK-NUMBER-GUESS/script.js
@@ -0,0 +1,31 @@
+const EsgEl = document.getElementById('msg');
+
+const randomNum = getRandomNumber();
+
+console.log('Number: ', randomNum);
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+
+// Start recognition and game
+recognition.start();
+
+// Capture user speak
+function onSpeak(e) {
+  const msg = e.results[0][0].transcript;
+  console.log('What did you say?:', msg);
+  // writeMessage(msg);
+  // checkNumber(msg)
+}
+
+// Generate random number
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+// Speck result
+
+recognition.addEventListener('result', onSpeak);
